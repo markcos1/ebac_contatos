ef2c93581dcf5f68b0eb767574c9c320c3253a27
marcos <marcos@exemplo.com>
2025-01-08 21:05:39 -0300

    jooj

--- main.js
+++ main.js
@@ -1,9 +1,7 @@
 const form = document.getElementById('contato-form');
 const tabelaDeContatos = document.getElementById('tabela-de-contatos');
 
-function nomeDoContato(nome){
-    const nomeComoArray = nome.split('')
-}
+
 
 
 
