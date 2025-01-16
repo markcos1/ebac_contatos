ad16f889fb3f2d3649e432363709a67d6afd30e6
marcos <marcos@exemplo.com>
2025-01-15 21:32:25 -0300

    frau

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
-document.getElementsByClassName('contato-form')
-document.getElementsByClassName('container');
+const form = document.getElementById('contato-form');
+
+function validaNome(nomeCompleto) {
+    const nomeComoArray = nomeCompleto.split(' ');
+    return nomeComoArray.length >= 2;
+}
+
+form.addEventListener('submit', function(e){
+    e.preventDefault();
+})
+
+console.log('form')
 
