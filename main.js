382d50c20478becf1ef36f1e3d9f72876e5782bc
marcos <marcos@exemplo.com>
2025-01-23 20:56:51 -0300

    novo commit

--- main.js
+++ main.js
@@ -1,13 +1,20 @@
 const form = document.getElementById('contato-form');
 
 function validaNome(nomeCompleto) {
+    const nomeDoContato = document.getElementById('nome');
     const nomeComoArray = nomeCompleto.split(' ');
     return nomeComoArray.length >= 2;
 }
 
 form.addEventListener('submit', function(e){
     e.preventDefault();
+
+    
+    const numeroDoContato = document.getElementById('telefone')
 })
+
+
+
 
 console.log('form')
 
