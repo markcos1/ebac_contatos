42567e5ee63316ea3fbf5d8ecab794cf437413e0
marcos <marcos@exemplo.com>
2025-01-28 21:27:20 -0300

    nova edição

--- main.js
+++ main.js
@@ -1,20 +1,27 @@
 const form = document.getElementById('contato-form');
+const imgContato = `<img src="./Screenshot 2024-11-15 at 20-50-06 Aplique efeitos e animações — EBAC LMS.png" id="imagem" />`;
 
-function validaNome(nomeCompleto) {
-    const nomeDoContato = document.getElementById('nome');
-    const nomeComoArray = nomeCompleto.split(' ');
-    return nomeComoArray.length >= 2;
-}
+let linhas = '';
 
 form.addEventListener('submit', function(e){
     e.preventDefault();
 
-    
-    const numeroDoContato = document.getElementById('telefone')
+    const imagemDoContato = document.getElementById('imagem');
+    const inputNomeDoContato = document.getElementById('nome');
+    const inputNumeroDoContato = document.getElementById('telefone');
+
+    let linha = '<p>';
+    linha += `<p>${imagemDoContato.value}</p>`
+    linha += `<p>${inputNomeDoContato.value}</p>`;
+    linha += `<p>${inputNumeroDoContato.value}</p>`;
+    linha += '</p>'
+
+    linhas += linha;
+
+    const corpoContato = document.getElementById('nome-contato');
+    corpoContato.innerHTML = linhas;
+
+    inputNomeDoContato.value = '';
+    inputNumeroDoContato.value = '';
+
 })
-
-
-
-
-console.log('form')
-
