87411f12c4d32ba426bcd4b57a61b155dcc081a8
marcos <marcos@exemplo.com>
2025-02-01 21:03:33 -0300

    Novo commit

--- main.js
+++ main.js
@@ -1,14 +1,25 @@
 const form = document.getElementById('contato-form');
 const imgContato = `<img src="./Screenshot 2024-11-15 at 20-50-06 Aplique efeitos e animações — EBAC LMS.png" id="imagem" >`;
+const nome = [];
+const telefone = [];
 
 let linhas = '';
 
 form.addEventListener('submit', function(e){
     e.preventDefault();
 
+    adicionaLinha();
+    atualizaTabela();
+});
+
+function adicionaLinha() {
+    
     const imagemDoContato = document.querySelector('img');
     const inputNomeDoContato = document.getElementById('nome');
     const inputNumeroDoContato = document.getElementById('telefone');
+
+    nome.push(inputNomeDoContato.value);
+    telefone.push(inputNumeroDoContato.value);
 
     let linha = '<p>';
     linha += `<p>${imagemDoContato.value}</p>`
@@ -18,10 +29,14 @@ form.addEventListener('submit', function(e){
 
     linhas += linha;
 
-    const corpoContato = document.querySelector('p');
-    corpoContato.innerHTML = linhas;
 
     inputNomeDoContato.value = '';
     inputNumeroDoContato.value = '';
+}
 
-})
+function atualizaTabela() {
+    
+    const corpoContato = document.querySelector('p');
+    corpoContato.innerHTML = linhas;
+}
+
